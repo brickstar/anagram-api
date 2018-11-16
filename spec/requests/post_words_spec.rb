737b92e89e8522ecf072b39b4c5f5a58e17fbd27
brickstar <brickstar@gmail.com>
2018-11-16 00:45:49 -0700

    Add words to post words spec

--- spec/requests/post_words_spec.rb
+++ spec/requests/post_words_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 describe "POST /words.json" do
   it "should add words to corpus" do
+    params = { "words": ["read", "dear", "dare"] }
+    words = params[:words]
 
     expect(Word.all.count).to eq(0)
-
-    params = { "words": ["read", "dear", "dare"] }
-
+    
     post "/words.json", params: params
 
     expect(Word.all.count).to eq(3)
+    expect(Word.pluck(:word)).to eq(words)
   end
 
 end
