9931fe45bb8081f28af7eff9a231734acc6c7b32
brickstar <brickstar@gmail.com>
2018-11-18 20:08:15 -0700

    Refactor anagram by_word_group_size

--- app/models/anagram.rb
+++ app/models/anagram.rb
@@ -22,13 +22,24 @@ class Anagram < ApplicationRecord
   end
 
   def self.by_word_group_size(limit)
-    anagrams = Anagram.includes(:words).where("words_count >= #{limit}").order(words_count: :desc)
-    grouped = anagrams.group_by { |anagram| anagram.words_count }
-    grouped.map do |words_count, anagram|
+    grouped_by_words_count(limit).map do |words_count, anagram|
       {
         words_count: words_count,
         words: anagram.map { |anagram| anagram.words.map { |word| word.word } }
       }
     end
   end
+
+  private
+
+    def self.grouped_by_words_count(limit)
+      anagrams_by_words_size(limit).group_by { |anagram| anagram.words_count }
+    end
+
+    def self.anagrams_by_words_size(limit)
+      includes(:words)
+      .where("words_count >= #{limit}")
+      .order(words_count: :desc)
+    end
+
 end
