a4beceab5195d2d0231cd486f53843d66bee6ea5
brickstar <brickstar@gmail.com>
2018-11-19 20:36:27 -0700

    Add words-analytics spec

--- spec/requests/words_request_spec.rb
+++ spec/requests/words_request_spec.rb
@@ -180,4 +180,24 @@ describe "Anagrams API" do
     end
   end
 
+  describe "GET /words-analytics" do
+    it "should return count of words and min/max/median/average word length" do
+      word_1 = Word.create(word: "read")
+      word_2 = Word.create(word: "reads")
+      word_3 = Word.create(word: "reader")
+
+      get '/words-analytics'
+      
+      expect(response).to be_successful
+
+      response = JSON.parse(body, symbolize_names: true)
+
+      expect(response).to have_key(:total_word_count)
+      expect(response).to have_key(:shortest_word)
+      expect(response).to have_key(:longest_word)
+      expect(response).to have_key(:median_word_length)
+      expect(response).to have_key(:mean_word_length)
+    end
+  end
+
 end
