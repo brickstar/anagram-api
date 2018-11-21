a9174fb99b3d5c2594292d7eff10a3349ce8ec47
brickstar <brickstar@gmail.com>
2018-11-20 21:51:01 -0700

    Refactor proper noun anagrams module

--- app/helpers/proper_noun_anagrams.rb
+++ app/helpers/proper_noun_anagrams.rb
@@ -1,13 +1,19 @@
 module ProperNounAnagrams
   def delete_proper_nouns
     if params[:proper_nouns] == "false"
-      anagrams = AnagramsPresenter.new.anagrams(params[:word], params[:limit], params[:proper_nouns])[:anagrams]
-      anagrams = anagrams.delete_if do |word|
-        capitalized?(word)
-      end
       render json: {
-                      anagrams: anagrams
+                      anagrams: without_proper_nouns
                    }
+    end
+  end
+
+  def get_anagrams
+    AnagramsPresenter.new.anagrams(params[:word], params[:limit], params[:proper_nouns])[:anagrams]
+  end
+
+  def without_proper_nouns
+    get_anagrams.delete_if do |word|
+      capitalized?(word)
     end
   end
 
