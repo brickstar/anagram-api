91f8b42137f892d17ab8214a8bf3645299eee7d1
brickstar <brickstar@gmail.com>
2018-11-24 18:39:39 -0700

    Remove proper noun validator module and before action. There are no proper nouns in the largest anagram set

--- app/controllers/most_anagrams_controller.rb
+++ app/controllers/most_anagrams_controller.rb
@@ -1,8 +1,7 @@
 class MostAnagramsController < ApplicationController
-  include ProperNounMost
-  before_action :delete_proper_nouns
-  
+
   def index
     render json: AnagramsPresenter.new.most_anagrams
   end
+  
 end
